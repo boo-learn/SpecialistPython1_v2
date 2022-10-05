9587defcdaeafc91bb42c933fd56acc8f2cdf63a
Mariia Manturova <114989728+MariiaManturova@users.noreply.github.com>
2022-10-05 18:12:16 +0300

    Update [hw]01_task_string.py

--- Module4/home_work/01_string/[hw]01_task_string.py
+++ Module4/home_work/01_string/[hw]01_task_string.py
@@ -2,3 +2,10 @@
 # Если точек и запятых одинаково - выведите "одинаково"
 
 text = ...
+text = "tfgsn/sdrthy./asdg./fglhnm,m,////..,,.,,"
+if text.count(",")>text.count("."):
+    print("запятых")
+elif text.count(",")<text.count("."):
+    print("точек")
+else:
+    print("одинаково")
