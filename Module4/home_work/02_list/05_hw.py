308691cd4070f97dba6f73454d90b68163f34b8e
th4ts <114989637+th4ts@users.noreply.github.com>
2022-10-05 20:38:33 +0300

    Update 05_hw.py

--- Module4/home_work/02_list/05_hw.py
+++ Module4/home_work/02_list/05_hw.py
@@ -2,5 +2,8 @@
 # Найдите самое длинное имя, если таких имен несколько - выведи любое их них
 
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
+names.sort(key=len, reverse=True)
+print(names[0])
+print(max(names, key=len))
 
 # TODO: your code here
