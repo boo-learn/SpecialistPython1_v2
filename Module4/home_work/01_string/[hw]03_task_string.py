25f5136c977350ed5103ae394518c0f8292d386c
Vladimir Lukin <114989907+Luque07@users.noreply.github.com>
2022-10-05 22:48:44 +0300

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,13 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = "Вася бил баклуши."
+consonants = "бвгджзйклмнпрстфхцчшщ"
+
+consonants_counter = 0
+i = 0
+while i < len(consonants):
+    consonants_counter = consonants_counter + text.lower().count(consonants[i])
+    i = i + 1
+
+print(consonants_counter)
+
