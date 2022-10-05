cd2f2539c083aefd5f234a25c150bc3d6ef32d3e
MelekhSergey <114989848+MelekhSergey@users.noreply.github.com>
2022-10-05 22:53:37 +0300

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,11 @@
 # Посчитайте количество согласных букв в данной строке.
+text = input("Введите произвольный текст: ")
+consonants = "чсмтьбфвпрлджцкнгшщзхъ"
 
-text = ...
+consonants_counter = 0
+i = 0
+while i < len(consonants):
+    consonants_counter += text.lower().count(consonants[i])
+    i += 1
+
+print(consonants_counter)
