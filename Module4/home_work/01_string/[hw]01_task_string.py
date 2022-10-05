5667fa0dd4be6155ff267c1f674c6fbee75ec8ca
dzhavaharnal <112697031+dzhavaharnal@users.noreply.github.com>
2022-10-05 13:49:38 +0300

    Update [hw]01_task_string.py

--- Module4/home_work/01_string/[hw]01_task_string.py
+++ Module4/home_work/01_string/[hw]01_task_string.py
@@ -2,3 +2,12 @@
 # Если точек и запятых одинаково - выведите "одинаково"
 
 text = ...
+number_of_dots = text.count('.')
+number_of_commas = text.count(',')
+
+if number_of_dots > number_of_commas:
+    print("Больше точек")
+elif number_of_commas > number_of_dots:
+    print("Больше запятых")
+else:
+    print("Точек и запятых одинаково")
