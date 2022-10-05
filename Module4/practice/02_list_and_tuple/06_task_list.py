c2eb0c415655486aed465905b7f04c6558aa8ea0
dzhavaharnal <112697031+dzhavaharnal@users.noreply.github.com>
2022-10-05 22:05:10 +0300

    Update 06_task_list.py

--- Module4/practice/02_list_and_tuple/06_task_list.py
+++ Module4/practice/02_list_and_tuple/06_task_list.py
@@ -4,4 +4,12 @@
 first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
-# TODO: your code here
+if first_number > second_number:
+    first_number, second_number = second_number, first_number
+
+numbers = []
+for i in range(first_number, second_number + 1):
+    if i % 3 == 0:
+        numbers.append(i)
+
+print(numbers)
