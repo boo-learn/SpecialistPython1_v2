c61518430f4c17958001ad3de2aaf11fc0526484
Sergeant1000 <114990210+Sergeant1000@users.noreply.github.com>
2022-10-05 21:37:44 +0300

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,16 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+word = input("Введите строку: ")
+vow = 0
+cons = 0
+for i in word:
+    letter = i.lower()
+    if letter == "а" or letter == "у" or \
+            letter == "о" or letter == "ы" or \
+            letter == "э" or letter == "ю" or \
+            letter == "я" or letter == "е" or \
+            letter == "и" or letter == "ё":
+        vow += 1
+    else:
+        cons += 1
+print("Гласные звуки %i\nСогласные звуки %i" % (vow, cons))
