145540fa09acfe5b904fcec9f5c6510c4878d6d6
FantasticDreamBird <114989892+FantasticDreamBird@users.noreply.github.com>
2022-10-06 00:45:56 +0300

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,10 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = "В теории, теория и практика неразделимы. На практике это не так. Но не факт."
+consonants = "йцкнгшщзхъфвпрлджчсмтьб"
+consonants_counter = 0
+i = 0
+while i < len(consonants):
+    consonants_counter += text.lower().count(consonants[i])
+    i += 1
+print(consonants_counter)
