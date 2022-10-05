48214dc758a30c3a6963580aa512d4e018447b18
Nnurislamova <114989817+Nnurislamova@users.noreply.github.com>
2022-10-05 19:12:59 +0300

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,9 @@
 # Посчитайте количество согласных букв в данной строке.
-
-text = ...
+text = "Бюджетные риски делятся на явные и неявные"
+notvowels = "бвгджзклмнпрстфхцчшщ"
+notvowels_counter = 0
+i = 0
+while i < len(notvowels) :
+    notvowels_counter += text.lower().count(notvowels[i])
+    i += 1
+print(notvowels_counter)
