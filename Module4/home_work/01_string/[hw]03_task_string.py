3e30cae0d8c6be57d5731a4b4f6f6a7a9bbdd120
Sakand <65117723+Sakand@users.noreply.github.com>
2022-10-06 00:31:08 +0300

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,12 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = "Посчитайте количество согласных букв в данной строке."
+
+consonants = 'бвгджзйклмнпрстфхцчшщ'
+count = 0
+for text_symb in text.lower():
+    for cons in consonants:
+        if text_symb == cons:
+            count += 1
+print(count)
+
