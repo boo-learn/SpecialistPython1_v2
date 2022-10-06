8890fb4f806e1142303d25c36e28af42fa8cc425
dzhavaharnal <112697031+dzhavaharnal@users.noreply.github.com>
2022-10-06 11:33:51 +0300

    Update 03_task_dict.py

--- Module5/practice/dict/03_task_dict.py
+++ Module5/practice/dict/03_task_dict.py
@@ -4,6 +4,6 @@
 keys = ['name', 'surname', 'age', 'rate']
 values = ['Петр', 'Первый', 42, 1300]
 
-# TODO: your code here
+my_dict = {key: values[i] for i, key in enumerate(keys)}
 # Нужно получить словарь:
 # {'name': 'Петр', 'surname': 'Первый', 'age': 42, 'rate': 1300}
