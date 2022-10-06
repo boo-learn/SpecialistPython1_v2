38cef00a7c40c9eafd4864a76bc6509456da8dbd
IvanDer009 <114989825+IvanDer009@users.noreply.github.com>
2022-10-07 00:29:11 +0300

    Update 04_hw_func.py

--- Module6/home_work/04_hw_func.py
+++ Module6/home_work/04_hw_func.py
@@ -9,6 +9,33 @@
 # Ввод: -2/3 - -2
 # Вывод: 1 1/3
 
-# TODO: your code here
+def plus_minus(a):
+    divide = a.split()
+    if divide[0].count('/'):
+        first = divide[0].split('/')
+    else:
+        first = [int(divide[0]) * int(divide[0]), int(divide[0])]
+    if divide[2].count('/'):
+        second = divide[2].split('/')
+    else:
+        second = [int(divide[2]) * int(divide[2]), int(divide[2])]
+    if divide[1] == '+':
+        num = int(first[0]) * int(second[1]) + int(first[1]) * int(second[0])
+    else:
+        num = int(first[0]) * int(second[1]) - int(first[1]) * int(second[0])
+    den = int(second[1]) * int(first[1])
+
+    if den < 0:
+        den *= -1
+        num *= -1
+    hcf = 1
+    for i in range(1, den):
+        if (num % den) % i == 0 and den % i == 0:
+            hcf = i
+    return f'{num // den} {int((num % den) / hcf)}/{int(den / hcf)}'
+
+
+text = '5/6 + 4/7'
+print(plus_minus(text))
 
 
