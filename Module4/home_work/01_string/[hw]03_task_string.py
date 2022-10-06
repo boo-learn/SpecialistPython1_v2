eb0555e574b883a65ce195de9691215975e3a9aa
AlexTmb78 <114989863+AlexTmb78@users.noreply.github.com>
2022-10-06 09:17:40 +0300

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,13 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = 'Отправлено уведомление о переводе служебной записки в статус "Завершена"'
+constants = "бвгджзйклмнпрстфхцчшщ"
+num_const = 0
+for letter in text:
+    if letter.isalpha():
+        for const in constants:
+            if letter == const:
+                num_const += 1
+                print(letter)
+print(num_const)
+
