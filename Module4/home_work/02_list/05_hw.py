a2cfc70848cc4033c7298bbb8c6b9d1e95d4e664
dzhavaharnal <112697031+dzhavaharnal@users.noreply.github.com>
2022-10-06 07:43:37 +0300

    Update 05_hw.py

--- Module4/home_work/02_list/05_hw.py
+++ Module4/home_work/02_list/05_hw.py
@@ -3,4 +3,12 @@
 
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
-# TODO: your code here
+max_length = 0
+longest_name = ''
+
+for name in names:
+    if len(name) > max_length:
+        max_length = len(name)
+        longest_name = name
+
+print(longest_name)
