f455aab86770cdc14bdcc3e0654640cb7dda60dc
Nnurislamova <114989817+Nnurislamova@users.noreply.github.com>
2022-10-06 14:03:59 +0300

    Update 08_task_tuple.py

--- Module4/practice/02_list_and_tuple/08_task_tuple.py
+++ Module4/practice/02_list_and_tuple/08_task_tuple.py
@@ -4,12 +4,12 @@
 tup1 = (2, 4, -6)
 tup2 = (2, "Василия", -6)
 tup3 = 2, 4, -6
-tup4 = (2)
+#tup4 = (2)
 tup5 = (2,)
 tup6 = 2,
 tup7 = tuple([2, 4, -6, 12])
-tup8 = tuple(2, 7, 8, -5)
+#tup8 = tuple(2, 7, 8, -5)
 tup9 = tuple()
 tup10 = tuple("hello")
 tup11 = ()
-tup12 = 2 and 4
+#tup12 = 2 and 4
