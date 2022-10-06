8c8b1b954c169cc89e9fd4f1d451c7199d261f3f
LukakhinAE <114989939+LukakhinAE@users.noreply.github.com>
2022-10-06 22:59:56 +0300

    Update 03_hw.py

--- Module4/home_work/02_list/03_hw.py
+++ Module4/home_work/02_list/03_hw.py
@@ -2,3 +2,15 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+from random import randint
+
+n = int(input("Введите желаемое количество элементов в списке: "))
+numbers = [randint(-100, 100) for i in range(n)]
+print(numbers)
+total = 0
+for number in numbers:
+    if number > 0 and number % 2 == 0:
+        total += number
+
+print("Сумма положительных чисел кратных двум равна:", total)
+
