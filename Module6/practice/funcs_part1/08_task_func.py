fb953bc19501ea419655131acce9029a620fb353
dzhavaharnal <112697031+dzhavaharnal@users.noreply.github.com>
2022-10-06 16:11:34 +0300

    Update 08_task_func.py

--- Module6/practice/funcs_part1/08_task_func.py
+++ Module6/practice/funcs_part1/08_task_func.py
@@ -1,1 +1,9 @@
 # Напишите функцию находящую n-ое число Фибоначчи.
+def fibbonachi(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibbonachi(n - 1) + fibbonachi(n - 2)
+
+print(fibbonachi(20))
