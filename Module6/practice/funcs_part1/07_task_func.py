08c951fcdd47234cb251817577c75baee9d2d839
Sakand <65117723+Sakand@users.noreply.github.com>
2022-10-06 16:37:58 +0300

    Update 07_task_func.py

--- Module6/practice/funcs_part1/07_task_func.py
+++ Module6/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,11 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+def time_format(sec):
+    seconds = sec % 60
+    minutes = sec % 3600 // 60
+    hours = sec // 3600
+    return f"{hours:0>2}:{minutes:0>2}:{seconds:0>2}"
+
+
+print(time_format(29143))
