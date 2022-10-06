a47829eaaeb6c41f4917b0d74d5b46f271b875c0
gremushka <42302302+gremushka@users.noreply.github.com>
2022-10-06 10:07:37 +0300

    Update 09_task_tuple.py

--- Module4/practice/02_list_and_tuple/09_task_tuple.py
+++ Module4/practice/02_list_and_tuple/09_task_tuple.py
@@ -1,4 +1,13 @@
 # Дано 3 кортежа заполненные произвольными целыми числами.
 # Найдите количество элементов, которые встречаются во всех трех кортежах.
 
-# TODO: your code here
+a = (2, 3, 5, 'gg', 'wp')
+b = (34, 23, 'gg', 'wp', 'ups')
+c = ('gg', 'wp', 45)
+
+counter = 0
+for element in a:
+    if (element in b) and (element in c):
+        counter += 1
+      
+print(counter)
