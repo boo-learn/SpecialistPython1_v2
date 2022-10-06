c24a63ab651833205a96cddaa225c55513b11917
dikolchi <114989665+dikolchi@users.noreply.github.com>
2022-10-06 17:40:52 +0300

    Update 05_hw.py

--- Module4/home_work/02_list/05_hw.py
+++ Module4/home_work/02_list/05_hw.py
@@ -2,5 +2,13 @@
 # Найдите самое длинное имя, если таких имен несколько - выведи любое их них
 
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
+max_len_name = 0
 
-# TODO: your code here
+for name in names:
+    if len(name) > max_len_name:
+        max_len_name = len(name)
+
+for name in names:
+    if len(name) == max_len_name:
+        print(name)
+        break
