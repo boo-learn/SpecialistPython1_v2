e5ab913915638ea7bdc8f22779bed1165fcd5224
FantasticDreamBird <114989892+FantasticDreamBird@users.noreply.github.com>
2022-10-06 09:59:05 +0300

    Update 02_hw.py

--- Module4/home_work/02_list/02_hw.py
+++ Module4/home_work/02_list/02_hw.py
@@ -3,6 +3,10 @@
 # Подсказка:
 # для получения случайного числа используйте функцию randint() модуля random
 import random
+n = int(input("n="))
 numbers = []
-# print(random.randint(10, 20))  # раскомментируйте, чтобы посмотреть работу функции randint
-# TODO: your code here
+i = 0
+while i < n:
+    numbers.append(random.randint(-100, 100))
+    i += 1
+print(numbers)
