70de8603d2cee035dfc3e5bef8d9e495eeae7a77
dikolchi <114989665+dikolchi@users.noreply.github.com>
2022-10-06 15:47:08 +0300

    Update 03_task_func.py

--- Module6/practice/funcs_part1/03_task_func.py
+++ Module6/practice/funcs_part1/03_task_func.py
@@ -2,9 +2,7 @@
 # Напишите функцию нахождения расстояния между двумя точками.
 
 def distance(x1, y1, x2, y2):
-    # TODO: your code here
-    pass
-
+    return ((x1 - x2)**2 + (y1 - y2)**2)**0.5
 
 # Тестируем функцию
 print(distance(2, 4, 2, 9))
