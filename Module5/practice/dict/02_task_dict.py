6016e8ad077990c39e9c255a5df2a5c33687a0b5
gremushka <42302302+gremushka@users.noreply.github.com>
2022-10-07 11:41:35 +0300

    Update 02_task_dict.py

--- Module5/practice/dict/02_task_dict.py
+++ Module5/practice/dict/02_task_dict.py
@@ -6,7 +6,18 @@ items = [
     {"name": "Чемодан", "price": "2300.85", "currency": "rub"},
 ]
 
-# 1. Выведите нумерованный список именований всех товаров
-...
-# 2. Выведите цену самого дешевого товара
-...
+items = [
+    {"name": "Кроссовки", "price": "7540.5", "currency": "rub"},
+    {"name": "Шорты", "price": "1313.2", "currency": "rub"},
+    {"name": "Кепка", "price": "738.0", "currency": "rub"},
+    {"name": "Чемодан", "price": "2300.85", "currency": "rub"},
+]
+counter=0
+minprice=items[0]
+for el in items:
+    if float(el['price'])<float(minprice['price']):
+        minprice=el
+    counter += 1
+    print(counter,el['name'])
+
+print(f"Минимальная цена у {minprice['name']} - {minprice['price']}")
