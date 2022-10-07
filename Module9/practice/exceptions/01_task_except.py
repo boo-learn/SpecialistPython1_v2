2a5fea2b5a7fa69acce767efcdf060162314167c
FantasticDreamBird <114989892+FantasticDreamBird@users.noreply.github.com>
2022-10-07 16:27:40 +0300

    Update 01_task_except.py

--- Module9/practice/exceptions/01_task_except.py
+++ Module9/practice/exceptions/01_task_except.py
@@ -4,3 +4,14 @@
 # На вход программе подается строка формата nxm (x - латинская буква икс).
 # Пример входных данных: 12x6
 # Если данные вводятся в неверном формате, сообщить об этом и запросить ввод заново.
+
+rectangle = input("nxm:")
+
+try:
+    if rectangle.find("x") == -1:
+        raise ValueError("value error")
+    else:
+        sides = rectangle.split("x")
+        print(int(sides[0]) // int(sides[1]))
+except ValueError as e:
+    print(e)
