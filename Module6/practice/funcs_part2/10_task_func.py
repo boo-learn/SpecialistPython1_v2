5b63b929b64a545aac5d3a5cd335b81bd39ce8ec
Xrom77 <114989786+Xrom77@users.noreply.github.com>
2022-10-07 10:28:42 +0300

    Update 10_task_func.py

--- Module6/practice/funcs_part2/10_task_func.py
+++ Module6/practice/funcs_part2/10_task_func.py
@@ -1,11 +1,15 @@
 # Напишите функцию, находящую среднее арифметическое всех аргументов
 # Функция должна вызываться с любым количеством аргументов
 
-def average(*args):
-    # TODO: your code here
-    pass
+# Напишите функцию, находящую среднее арифметическое всех аргументов
+# Функция должна вызываться с любым количеством аргументов
 
+def average(*args):
+    total=0
+    for numbers in args:
+       total=total+numbers
+    return total/len(args)
 
 print(average(3, 4, 8))
 print(average(1, 4, 5, -3, 8, 4))
-print(average(-10, 12, 6.3, -5.5, 7, 0.2))
+print(average(-10, 12, 6.3, -5.5, 7, 0.2)))
