3e26f5ba3765818cb9eea3c303c5a11db281f5c2
IvanDer009 <114989825+IvanDer009@users.noreply.github.com>
2022-10-07 11:50:34 +0300

    Update 02_task_dict.py

--- Module5/practice/dict/02_task_dict.py
+++ Module5/practice/dict/02_task_dict.py
@@ -5,8 +5,14 @@ items = [
     {"name": "Кепка", "price": "738.0", "currency": "rub"},
     {"name": "Чемодан", "price": "2300.85", "currency": "rub"},
 ]
-
 # 1. Выведите нумерованный список именований всех товаров
-...
+low_price = 1e9
+num = 1
+for i in items:
+    print(num, i["name"])
+    if float(i["price"]) < low_price:
+        low_price = float(i["price"])
+    num += 1
+
 # 2. Выведите цену самого дешевого товара
-...
+print('Самый дешевый: ', low_price)
