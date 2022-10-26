0b0be44696148f7f768acfc22b51d340025c10c0
MaksimochkinBogdan <116176291+MaksimochkinBogdan@users.noreply.github.com>
2022-10-26 23:46:41 +0300

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,13 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = "ааа ввв ббб ооо ууу"
+
+i = 0
+summa_consonant = 0
+summa_no = 0
+
+while i < len(text):
+    if text[i] == "б" or text[i] == "в" or text[i] == "г" or text[i] == "д"  or text[i] == "ж":
+        summa_consonant = summa_consonant + 1
+    i = i + 1
+print(summa_consonant)
