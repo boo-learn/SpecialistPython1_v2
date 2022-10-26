eafe67eb3cd94219a88fd79ca2e3e252c80474f0
Fenixz480 <116557823+Fenixz480@users.noreply.github.com>
2022-10-26 21:53:29 +0300

    Update 04_hw.py

--- Module4/home_work/02_list/04_hw.py
+++ Module4/home_work/02_list/04_hw.py
@@ -5,3 +5,10 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+numbers = [-4, 19, 22, 20, 19, 9, 25, 16, 4]
+new_list = []
+for number in numbers:
+    if number > 0 and (number ** 0.5) % 1 == 0:
+        a = int(number ** (0.5))
+        new_list.append(a)
+print(new_list)
