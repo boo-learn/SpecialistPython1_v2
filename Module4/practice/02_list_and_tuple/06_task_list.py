619f23f80d9ad5e7dfe92bece59f60d2234667e0
filinrun <116560638+filinrun@users.noreply.github.com>
2022-10-26 14:12:14 +0300

    Update 06_task_list.py

--- Module4/practice/02_list_and_tuple/06_task_list.py
+++ Module4/practice/02_list_and_tuple/06_task_list.py
@@ -4,4 +4,18 @@
 first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
-# TODO: your code here
+# TODO:
+first_number = int(input())
+second_number = int(input())
+list_nums=[first_number]
+i=first_number
+while i<second_number:
+    list_nums.append(i+1)
+    i+=1
+print(list_nums)
+new_list=[0]
+for num in list_nums:
+    if num%3==0:
+        new_list.append(num)
+new_list.pop(0)
+print(new_list)
