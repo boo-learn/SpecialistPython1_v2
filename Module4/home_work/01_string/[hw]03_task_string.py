ddb2866411e9d1f78d10520ad4582e46a53d1d3e
filinrun <116560638+filinrun@users.noreply.github.com>
2022-10-26 09:45:23 +0300

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,12 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+check_string = "Посчитайте количество согласных букв в данной строке"
+slovo=check_string.lower()
+i=0
+total=0
+while i<len(check_string):
+    simv=slovo[i:i+1]
+    i+=1
+    if simv=="б"or"в"or"г"or"д"or"ж"or"з"or"й"or"к"or"л"or"м"or"н"or"п"or"р"or"с"or"т"or"в":
+        total+=1
+print(total)
