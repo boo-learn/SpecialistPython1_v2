3463630ec1000761f6528ee785f5b7ad47b998ab
naste4kina <39644928+naste4kina@users.noreply.github.com>
2022-10-26 12:58:16 +0300

    Update 06_task_list.py

--- Module4/practice/02_list_and_tuple/06_task_list.py
+++ Module4/practice/02_list_and_tuple/06_task_list.py
@@ -4,4 +4,14 @@
 first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
-# TODO: your code here
+
+if first_number < second_number:
+    for i in range(first_number, (second_number + 1)):
+        if i % 3 == 0:
+            print(i)
+
+elif first_number > second_number:
+    for i in range(second_number, (first_number + 1)):
+        if i % 3 == 0:
+            print(i)
+
