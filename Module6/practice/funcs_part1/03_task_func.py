a78c5df4b6315b45c21d38d09a6fb199d1d9fce2
IlsurG <116557913+IlsurG@users.noreply.github.com>
2022-10-27 13:58:22 +0300

    Update 03_task_func.py

--- Module6/practice/funcs_part1/03_task_func.py
+++ Module6/practice/funcs_part1/03_task_func.py
@@ -2,8 +2,11 @@
 # Напишите функцию нахождения расстояния между двумя точками.
 
 def distance(x1, y1, x2, y2):
-    # TODO: your code here
-    pass
+    coordinatesx = (x1, x2)
+    coordinatesy = (y1, y2)
+    dist = ((float(coordinatesx[0]) - float(coordinatesx[1])) ** 2 + (
+                float(coordinatesy[0]) - float(coordinatesy[1])) ** 2) ** 0.5
+    return dist
 
 
 # Тестируем функцию
