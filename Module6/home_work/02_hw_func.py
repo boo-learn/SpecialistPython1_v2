2be5ef7d84e27fbaca3c9149499a210a1f96c823
MaksimochkinBogdan <116176291+MaksimochkinBogdan@users.noreply.github.com>
2022-10-28 00:15:02 +0300

    Update 02_hw_func.py

--- Module6/home_work/02_hw_func.py
+++ Module6/home_work/02_hw_func.py
@@ -9,5 +9,32 @@ def distance(x1, y1, x2, y2):
     pass
 
 
-# TODO: your code here
+def distance(x1, y1, x2, y2):
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+ab = distance(5, 7, 9, 12)
+bc = distance(9, 14, 9, 12)
+ac = distance(12, 7, 11, 12)
+
+print(ab)
+print(bc)
+print(ac)
+
+min_n = ab
+i = 0
+for n in (ab, bc, ac):
+    if ab < bc and ab < ac:
+      min_n = ab
+    elif bc < ab and bc < ac:
+       min_n = bc
+    else: min_n = ac
+i = i + 1
+#print(min_n)
+if min_n == ab:
+    print("Самый короткий отрезок ab, равный", ab)
+elif min_n == bc:
+    print("Самый короткий отрезок bc, равный", bc)
+else:
+    print("Самый короткий отрезок ac, равный", ac)
+
 print("Самый короткий отрезок:", ...)  # Выводим название отрезка, например “АС”.
