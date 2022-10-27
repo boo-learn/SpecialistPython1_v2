6fb927c380ad2f20e3d1a0d5f3462d1775be97f5
MaksimochkinBogdan <116176291+MaksimochkinBogdan@users.noreply.github.com>
2022-10-27 10:17:07 +0300

    Update 03_hw.py

--- Module4/home_work/02_list/03_hw.py
+++ Module4/home_work/02_list/03_hw.py
@@ -1,4 +1,15 @@
 # Дан список из n элементов, заполненный произвольными целыми числами в диапазоне от -100 до 100.
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
-# TODO: your code here
+first_number = -100     # Первое число
+last_number = 100
+
+max_el = first_number
+summa_el = 0
+
+for el in range(first_number, last_number):
+    if el >= 0:
+        if el > max_el:
+            max_el = el
+            summa_el = summa_el + max_el
+print(summa_el)
