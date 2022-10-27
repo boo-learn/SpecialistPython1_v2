20958ebdcd4159321531dd6c3920afbba085d163
MaksimochkinBogdan <116176291+MaksimochkinBogdan@users.noreply.github.com>
2022-10-27 12:47:46 +0300

    Update 03_task_func.py

--- Module6/practice/funcs_part1/03_task_func.py
+++ Module6/practice/funcs_part1/03_task_func.py
@@ -1,9 +1,12 @@
 # Даны координаты двух точек на плоскости.
 # Напишите функцию нахождения расстояния между двумя точками.
-
 def distance(x1, y1, x2, y2):
-    # TODO: your code here
-    pass
+def distance(x1, y1, x2, y2):
+    lx = abs(x2 - x1)
+    ly = abs(y2 - y1)
+    return lx, ly
+
+
 
 
 # Тестируем функцию
