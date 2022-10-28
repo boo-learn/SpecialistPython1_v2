f8580b0b3ada395d7920fecd2ded6fd33f4fa018
IlsurG <116557913+IlsurG@users.noreply.github.com>
2022-10-28 09:56:42 +0300

    Update 03_hw_func.py

--- Module6/home_work/03_hw_func.py
+++ Module6/home_work/03_hw_func.py
@@ -1,4 +1,16 @@
 # Даны координаты центров двух окружностей (x1; y1) (x2; y2) и и их радиусы  R1 и R2.
 # Находится ли одна окружность целиком внутри другой
 
-# TODO: your code here
+def distance(x1, y1, x2, y2):
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+
+def circle_in_circle(x1, y1, x2, y2, r1, r2):
+    dist = distance(x1, y1, x2, y2)
+    min_r = min(r1, r2)
+    max_r = max(r1, r2)
+    if dist + min_r <= max_r:
+        print("Внутри")
+    else:
+        print("Убежал")
+    return
