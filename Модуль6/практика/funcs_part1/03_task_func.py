2d088309b6333ade196dcc9820eb53daeecb3b17
Ruslan115 <117640953+Ruslan115@users.noreply.github.com>
2022-11-09 17:02:53 +0300

    Update and rename Module6/practice/funcs_part1/03_task_func.py to Модуль6/практика/funcs_part1/03_task_func.py

--- Модуль6/практика/funcs_part1/03_task_func.py
+++ Модуль6/практика/funcs_part1/03_task_func.py
@@ -10,3 +10,10 @@ def distance(x1, y1, x2, y2):
 print(distance(2, 4, 2, 9))
 print(distance(12, 8, 12, -9))
 print(distance(23, 0, 15, 32))
+
+
+f point(x1, y1, x2, y2):
+    dist = ((x2-x1) ** 2 + (y2-y1) ** 2) ** 0.5
+    return dist
+
+
