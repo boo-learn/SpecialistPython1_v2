4d18b91218ee40a18d806786a1490ade2935620e
dEshex <117657281+dEshex@users.noreply.github.com>
2022-11-09 12:06:14 +0300

    Update 03_task_list.py

--- Module4/practice/02_list_and_tuple/03_task_list.py
+++ Module4/practice/02_list_and_tuple/03_task_list.py
@@ -2,3 +2,9 @@
 # Вывести на экран сумму всех элементов.
 
 # TODO: your code here
+number_list = [5, 7, 9, -10, 0, 3]
+sum_list_numbers = 0
+
+for i_number in number_list:
+    sum_list_numbers += i_number
+print("Сумма значений равна", sum_list_numbers)
