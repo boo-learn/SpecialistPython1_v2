5330dcfb3af2119db54f98611a927e3078c36bae
Aleksey Ilyenko <117640792+letsmoto@users.noreply.github.com>
2022-11-09 12:15:31 +0300

    Update 05_task_list.py

--- Module4/practice/02_list_and_tuple/05_task_list.py
+++ Module4/practice/02_list_and_tuple/05_task_list.py
@@ -6,7 +6,18 @@
 fruits = ["яблоко", "банан", "киви", "арбуз"]
 
 # TODO: your code here
+fruits = ["яблоко", "банан", "киви", "арбуз"]
+max_len_word = 0
 
+for fruit in fruits:
+    if len(fruit) > max_len_word:
+        max_len_word = len(fruit)
+
+max_string = max_len_word + len("x. ")
+for fruit in enumerate(fruits):
+    probels = " " * (max_string - 2 - len(fruit[1]))
+    print(str(fruit[0] + 1) + "." + probels + fruit[1])
+    
 # Пример вывода:
 # 1. яблоко
 # 2.  банан
