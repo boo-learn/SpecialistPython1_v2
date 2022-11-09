fe568e2ccd78b63461f0789bf1c6d79f48c20cfb
Guzel-Valieva <117732305+Guzel-Valieva@users.noreply.github.com>
2022-11-09 14:38:43 +0400

    Update 06_task_list.py

--- Module4/practice/02_list_and_tuple/06_task_list.py
+++ Module4/practice/02_list_and_tuple/06_task_list.py
@@ -5,3 +5,8 @@ first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
 # TODO: your code here
+res_list = []
+for num in range(first_number + 1, second_number):
+    if num % 3 == 0 and num != 0:
+        res_list.append(num)
+print(res_list)
