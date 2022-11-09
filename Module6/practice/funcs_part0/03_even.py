da0606f7bec9ea1c1f46e3cb430a71aff3ec953c
Pauline Tolstova <60112387+Tonilyn@users.noreply.github.com>
2022-11-09 16:02:49 +0300

    Update 03_even.py

--- Module6/practice/funcs_part0/03_even.py
+++ Module6/practice/funcs_part0/03_even.py
@@ -1,9 +1,12 @@
-def even(...):
-    ...
-    return ...
+def even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
 
-n = ...
+
+n = int(input("n: "))
 if even(n):
-   print("Число четное")
+    print("Число четное")
 else:
-   print("Число не четное")
+    print("Число не четное")
