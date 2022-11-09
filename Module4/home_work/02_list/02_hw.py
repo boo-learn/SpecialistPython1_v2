1dbf1db31cce84bc1c25b8bbbf86264c05c6ad96
Pauline Tolstova <60112387+Tonilyn@users.noreply.github.com>
2022-11-09 23:15:50 +0300

    Update 02_hw.py

--- Module4/home_work/02_list/02_hw.py
+++ Module4/home_work/02_list/02_hw.py
@@ -3,6 +3,14 @@
 # Подсказка:
 # для получения случайного числа используйте функцию randint() модуля random
 import random
+
+n = int(input("Число элементов в списке: "))
 numbers = []
+
+for i in range(n):
+    random_number = random.randint(-100, 100)
+    numbers.append(random_number)
+
+print(numbers)
 # print(random.randint(10, 20))  # раскомментируйте, чтобы посмотреть работу функции randint
 # TODO: your code here
