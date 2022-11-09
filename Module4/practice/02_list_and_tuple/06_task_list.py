88c36bf9819a137ba04147b6b4dfb8fbd8cfb032
Pauline Tolstova <60112387+Tonilyn@users.noreply.github.com>
2022-11-09 13:01:41 +0300

    Update 06_task_list.py

--- Module4/practice/02_list_and_tuple/06_task_list.py
+++ Module4/practice/02_list_and_tuple/06_task_list.py
@@ -1,7 +1,19 @@
 # Даны два целых числа.
 # Вывести список всех чисел кратных трем в диапазоне между заданными числами.
+import random
 
-first_number = int(input())     # Первое число
-second_number = int(input())    # Второе число
+first_number = int(input("Начало диапазона: "))     # Первое число
+second_number = int(input("Конец диапазона: "))    # Второе число
+n = int(input("Кол-во элементов в диапазоне: "))                # Желаемое кол-во чисел в диапазоне
+num_range = []
+final_list = []
 
-# TODO: your code here
+for i in range(n):
+    random_numbers = random.randint(first_number, second_number)
+    num_range.append(random_numbers)
+
+for number in num_range:
+    if number % 3 == 0:
+        final_list.append(number)
+
+print("Числа из диапазона, кратные 3: ", final_list)
