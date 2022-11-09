96f8b23059bdf7d52e71e133317357d2004702da
Genom09 <117641033+Genom09@users.noreply.github.com>
2022-11-09 22:48:12 +0300

    Update 08_task_func.py

--- Module6/practice/funcs_part1/08_task_func.py
+++ Module6/practice/funcs_part1/08_task_func.py
@@ -1,1 +1,12 @@
 # Напишите функцию находящую n-ое число Фибоначчи.
+def fib(n):
+    if n < 1:
+        return n % 2
+    if n <= 2:
+        return 1
+    return fib(n - 2) + fib(n - 1)
+
+
+print(fib(15))
+print(fib(1))
+print(fib(10))
