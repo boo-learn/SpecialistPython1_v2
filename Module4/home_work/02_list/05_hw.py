ad81b3fbc33a92d5b26861348e0079a5845bd5ed
dEshex <117657281+dEshex@users.noreply.github.com>
2022-11-09 22:44:48 +0300

    Update 05_hw.py

--- Module4/home_work/02_list/05_hw.py
+++ Module4/home_work/02_list/05_hw.py
@@ -2,5 +2,11 @@
 # Найдите самое длинное имя, если таких имен несколько - выведи любое их них
 
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
+max_len_name = 0
+name = ""
 
-# TODO: your code here
+for list_name in names:
+    if max_len_name < len(list_name):
+        max_len_name = len(list_name)
+        name = list_name
+print(name)
