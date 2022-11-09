c8f32b8a30722ceca0fcf945150b2fc46790c331
MaksAstakhov <117641431+MaksAstakhov@users.noreply.github.com>
2022-11-09 14:29:51 +0300

    Update 06_task_list.py

--- Module4/practice/02_list_and_tuple/06_task_list.py
+++ Module4/practice/02_list_and_tuple/06_task_list.py
@@ -4,4 +4,11 @@
 first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
-# TODO: your code here
+mylist = []
+a = int(input('A: '))
+b = int(input('B: '))
+while a <= b:
+    if a % 3 == 0:
+        mylist.append(a)
+    a += 1
+print(mylist)
