9a6ef980ddaa4b2b693b332839840ef237bea329
Genom09 <117641033+Genom09@users.noreply.github.com>
2022-11-09 16:15:58 +0300

    Update 01_task_func.py

--- Module6/practice/funcs_part1/01_task_func.py
+++ Module6/practice/funcs_part1/01_task_func.py
@@ -1,11 +1,11 @@
 # Напишите функцию, возвращающую наибольшее из четырех чисел
 
 def max4(n1, n2, n3, n4):
-    # TODO: your code here
-    pass
+    def max_my(a,b):
+        return a if a>b else b
+    #return max((n1, n2, n3, n4))
+    return max_my(max_my(n1,n2),max_my(n3,n4))
 
-
-# Тестируем функцию
 print(max4(5, 6, 12, 7))
 print(max4(-10, -12, -4, -9))
 print(max4(2.5, 2.6, 2.6, 2.4))
