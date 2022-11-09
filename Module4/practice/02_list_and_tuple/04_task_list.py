1737bfbcef8a71c7ce5e6487a88b25d61cae334c
Aleksey Ilyenko <117640792+letsmoto@users.noreply.github.com>
2022-11-09 12:33:08 +0300

    Update 04_task_list.py

--- Module4/practice/02_list_and_tuple/04_task_list.py
+++ Module4/practice/02_list_and_tuple/04_task_list.py
@@ -2,3 +2,9 @@
 # Вывести на экран сумму всех положительных элементов.
 
 # TODO: your code here
+my_list = [2, 5, 7, 13, 0, -2]
+total = 0
+for element in my_list:
+    if element > 0:
+        total += element
+print(total)
