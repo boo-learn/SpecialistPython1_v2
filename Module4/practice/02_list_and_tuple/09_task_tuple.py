46c4885cf1ace50d15f8d136fac0dda669ad9e78
Aleksey Ilyenko <117640792+letsmoto@users.noreply.github.com>
2022-11-09 12:59:05 +0300

    Update 09_task_tuple.py

--- Module4/practice/02_list_and_tuple/09_task_tuple.py
+++ Module4/practice/02_list_and_tuple/09_task_tuple.py
@@ -2,3 +2,14 @@
 # Найдите количество элементов, которые встречаются во всех трех кортежах.
 
 # TODO: your code here
+tuple1 = 1,3,54,3,44,21,3
+tuple2 = 9,3,1,6,34,7,3,2
+tuple3 = 4,3,5,1,7,5,8,9
+
+matches = []
+for t1 in tuple1:
+    for t2 in tuple2:
+        for t3 in tuple3:
+            if t1 == t2 == t3 and t1 not in matches:
+                matches.append((t1))
+print(matches)
