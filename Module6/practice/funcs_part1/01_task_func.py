a623c9c3be3b49e9959bd09c18eedfc6df87b43f
Eduard <99264101+youArgument@users.noreply.github.com>
2022-11-09 16:32:19 +0300

    Update 01_task_func.py

--- Module6/practice/funcs_part1/01_task_func.py
+++ Module6/practice/funcs_part1/01_task_func.py
@@ -1,8 +1,19 @@
 # Напишите функцию, возвращающую наибольшее из четырех чисел
 
 def max4(n1, n2, n3, n4):
-    # TODO: your code here
-    pass
+    def max2(n1, n2, n3, n4):
+    all_numb=[n1,n2,n3,n4]
+    max_el = all_numb[0]
+    for el in all_numb:
+        if el>max_el:
+            max_el = el
+    return max_el
+a = 5
+b = 6
+c = 12
+d = 7
+print(max2(a, b, c, d))
+
 
 
 # Тестируем функцию
