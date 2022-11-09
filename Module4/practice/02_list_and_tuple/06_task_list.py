71d23cef8ac3092b11048ac946fd716d2713fee4
Aleksey Ilyenko <117640792+letsmoto@users.noreply.github.com>
2022-11-09 12:52:05 +0300

    Update 06_task_list.py

--- Module4/practice/02_list_and_tuple/06_task_list.py
+++ Module4/practice/02_list_and_tuple/06_task_list.py
@@ -5,3 +5,9 @@ first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
 # TODO: your code here
+first_number = int(input())     # Первое число
+second_number = int(input())    # Второе число
+
+for number in range(first_number, second_number + 1):
+    if number % 3 == 0:
+        print(number)
