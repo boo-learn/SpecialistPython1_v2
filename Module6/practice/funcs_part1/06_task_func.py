c3a7956c0c3402022804e2e50516262c1fa2c2cd
Pauline Tolstova <60112387+Tonilyn@users.noreply.github.com>
2022-11-10 12:09:08 +0300

    Update 06_task_func.py
    Не успела понять, почему после результата еще выводится дополнительный None

--- Module6/practice/funcs_part1/06_task_func.py
+++ Module6/practice/funcs_part1/06_task_func.py
@@ -1,8 +1,21 @@
 # Напишите функцию, которая проверит, что точка (x, y)
 # находится строго внутри окружности с центром в точке (xc, yc) и радиусом r:
 
+point_x = int(input("Введите координату x точки: "))
+point_y = int(input("Введите координату y точки: "))
+circle_x = int(input("Введите координату x центра окружности: "))
+circle_y = int(input("Введите координату y центра окружности: "))
+circle_r = int(input("Введите радиус окружности: "))
+
+
 def point_in_circle(x, y, xc, yc, r):
-    # TODO: your code here
-    pass
+    distance = ((x - y) ** 2 + (yc - xc) ** 2) ** 0.5
+    if distance <= r:
+        print("Точка принадлежит кругу")
+    else:
+        print("Точка не принадлежит кругу")
+
+
+print(point_in_circle(point_x, point_y, circle_x, circle_y, circle_r))
 
 # Не забудьте протестировать вашу функцию
