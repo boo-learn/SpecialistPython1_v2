7442a5f20b3af42e98f5a56ea87bcfab3bbb3c24
Guzel-Valieva <117732305+Guzel-Valieva@users.noreply.github.com>
2022-11-10 21:47:36 +0400

    Update 01_hw_func.py

--- Module6/home_work/01_hw_func.py
+++ Module6/home_work/01_hw_func.py
@@ -3,8 +3,20 @@
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 
 def lucky_ticket(ticket_number):
-    # TODO: your code here
-    pass
+    if len(str(ticket_number)) != 6:
+        return "номер билета должен состоять из 6 цифр"
+    else:
+        num_list = list(str(ticket_number))
+        sum_first_half = 0
+        sum_second_half = 0
+        for num in num_list[:3]:
+            sum_first_half += int(num)
+        for num in num_list[3:]:
+            sum_second_half += int(num)
+        if sum_first_half == sum_second_half:
+            return True
+        else:
+            return False
 
 
 # Тестируем функцию
