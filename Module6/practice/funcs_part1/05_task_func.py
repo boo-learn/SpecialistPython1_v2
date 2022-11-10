5d99d74a827dd982f673b6387f386ff035a946a9
Pauline Tolstova <60112387+Tonilyn@users.noreply.github.com>
2022-11-10 12:01:14 +0300

    Update 05_task_func.py

--- Module6/practice/funcs_part1/05_task_func.py
+++ Module6/practice/funcs_part1/05_task_func.py
@@ -3,7 +3,31 @@
 # Если по заданным точкам треугольник построить нельзя, выведите соответствующее сообщение.
 # Подсказка: для нахождения площади используйте Теорему Герона
 
-# TODO: your code here
+xa = int(input())
+ya = int(input())
+xb = int(input())
+yb = int(input())
+xc = int(input())
+yc = int(input())
+
+
+def heron_formula(ab, bc, ca):
+    p = (ab + bc + ca) * 0.5
+    s = (p * (p - ab) * (p - bc) * (p - ca)) ** 0.5
+    return s
+
+
+side_ab = ((xb - xa) ** 2 + (yb - ya) ** 2) ** 0.5
+side_bc = ((xc - xb) ** 2 + (yc - yb) ** 2) ** 0.5
+side_ca = ((xa - xc) ** 2 + (ya - yc) ** 2) ** 0.5
+
+if (side_ab < side_bc + side_ca) \
+        and (side_bc < side_ab + side_ca) \
+        and (side_ca < side_ab + side_bc):
+    s_triangle = heron_formula(side_ab, side_bc, side_ca)
+    print(f"{s_triangle:.2f}")
+else:
+    print("Треугольника не существует")
 
 
 # Не забудьте протестировать вашу функцию
