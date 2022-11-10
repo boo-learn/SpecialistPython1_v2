4ee6e75c81b264578724f711f9dd615b17527989
Ruslan115 <117640953+Ruslan115@users.noreply.github.com>
2022-11-11 01:46:48 +0300

    Update and rename Module6/home_work/02_hw_func.py to Модуль6/домашняя работа/02_hw_func.py

--- Модуль6/домашняя работа/02_hw_func.py
+++ Модуль6/домашняя работа/02_hw_func.py
@@ -11,3 +11,33 @@ def distance(x1, y1, x2, y2):
 
 # TODO: your code here
 print("Самый короткий отрезок:", ...)  # Выводим название отрезка, например “АС”.
+
+
+def dist(x1, y1, x2, y2):
+    d = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+    return d
+
+
+def long(*args):
+    min_long = min(ab, bc, ac)
+    return min_long
+
+
+point1_x1 = int(input("x1 "))
+point1_y1 = int(input("y1 "))
+point2_x2 = int(input("x2 "))
+point2_y2 = int(input("y2 "))
+point3_x3 = int(input("x3 "))
+point3_y3 = int(input("y3 "))
+
+ab = dist(point1_x1, point1_y1, point2_x2, point2_y2)
+bc = dist(point2_x2, point2_y2, point3_x3, point3_y3)
+ac = dist(point3_x3, point3_y3, point1_x1, point1_y1)
+
+if ab == long():
+    print("ab минимальный")
+
+elif bc == long():
+    print("bc минимальный")
+else:
+    print("ac минимальный")
