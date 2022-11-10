b960ed86e10021e1fb2126a4a3cabe053c6d9696
Ruslan115 <117640953+Ruslan115@users.noreply.github.com>
2022-11-11 00:04:07 +0300

    Update and rename Module6/home_work/01_hw_func.py to Модуль6/домашняя работа/01_hw_func.py

--- Модуль6/домашняя работа/01_hw_func.py
+++ Модуль6/домашняя работа/01_hw_func.py
@@ -11,3 +11,12 @@ def lucky_ticket(ticket_number):
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
 print(lucky_ticket(436751))
+
+
+def lucky(x):
+    return x // 100000 + x // 10000 % 10 == x % 100 // 10 + x % 10
+
+
+print(lucky(115511))
+print(lucky(124321))
+print(lucky(436751))
