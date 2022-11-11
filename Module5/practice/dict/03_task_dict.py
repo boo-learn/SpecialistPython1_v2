35cf6263bd4268f7ea634d3b49d43261c0f32267
GaStaanVoor <64048521+GaStaanVoor@users.noreply.github.com>
2022-11-11 21:21:41 +1000

    Update 03_task_dict.py

--- Module5/practice/dict/03_task_dict.py
+++ Module5/practice/dict/03_task_dict.py
@@ -4,6 +4,8 @@
 keys = ['name', 'surname', 'age', 'rate']
 values = ['Петр', 'Первый', 42, 1300]
 
-# TODO: your code here
-# Нужно получить словарь:
+dictionary = dict()
+for key, value in zip(keys, values):
+    dictionary[key] = value
+print(dictionary)
 # {'name': 'Петр', 'surname': 'Первый', 'age': 42, 'rate': 1300}
