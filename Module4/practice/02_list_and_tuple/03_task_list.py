551e4928c0b690058349837f1e95db70f9f692b6
SherGrigoriy <125800297+SherGrigoriy@users.noreply.github.com>
2023-03-05 14:37:59 +0300

    Update 03_task_list.py

--- Module4/practice/02_list_and_tuple/03_task_list.py
+++ Module4/practice/02_list_and_tuple/03_task_list.py
@@ -2,3 +2,8 @@
 # Вывести на экран сумму всех элементов.
 
 # TODO: your code here
+some_list = [4, 1, 5, 4, 10, 4, -8, 3, -7]
+sum_elements = 0
+for i in some_list:
+    sum_elements += i
+print(sum_elements)
