feba6d6483569f1644aac5011107284d0d9c76c8
nudner80 <125882617+nudner80@users.noreply.github.com>
2023-03-06 16:34:43 +0300

    Update [hw]01_task_string.py

--- Module4/home_work/01_string/[hw]01_task_string.py
+++ Module4/home_work/01_string/[hw]01_task_string.py
@@ -2,3 +2,14 @@
 # Если точек и запятых одинаково - выведите "одинаково"
 
 text = ...
+text = input("Введите текст: ")
+
+num_points = text.count(".")
+num_commas = text.count(",")
+
+if num_commas > num_points:
+    print("Больше запятых")
+elif num_commas == num_points:
+    print("Запятых и точек одинаковое количество")
+else:
+    print("Больше точек")
