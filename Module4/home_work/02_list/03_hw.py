f21bea310650922162bd8554ec4ccf3e46ccd027
kochergin579 <120784174+kochergin579@users.noreply.github.com>
2023-03-06 11:26:08 +0300

    Update 03_hw.py

--- Module4/home_work/02_list/03_hw.py
+++ Module4/home_work/02_list/03_hw.py
@@ -2,3 +2,10 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+numbers = [10, -15, 2, 25, -85, -68, 12]
+
+summa_plus_elements = 0
+for number in numbers:
+    if number > 0 and number%2 == 0:
+        summa_plus_elements += number
+print(summa_plus_elements)
