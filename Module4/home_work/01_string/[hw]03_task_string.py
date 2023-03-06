a1059344ce6b0f78081233492b0df83762959839
kochergin579 <120784174+kochergin579@users.noreply.github.com>
2023-03-06 10:21:03 +0300

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,11 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = "I was searching my source to make a big desk yesterday."
+
+count = 0
+text1 = text.lower()
+vowels = set("aeiouy")
+for letter in text1:
+    if letter in vowels:
+        count += 1
+print("Количество гласных равно: ", count)
