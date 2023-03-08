7f909e39db398d2773e815fbeb633078e6325175
nudner80 <125882617+nudner80@users.noreply.github.com>
2023-03-08 10:33:05 +0300

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,16 @@
 # Посчитайте количество согласных букв в данной строке.
 
 text = ...
+text = input("Введите текст строки: ")
+f = filter(str.isalpha, text)
+new_text = "".join(f).lower()
+vse_gls = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"]
+sogls = 0
+
+for letter in new_text:
+    if letter in vse_gls:
+        sogls += 0
+    else:
+        sogls += 1
+
+print("В строке", sogls, "согласных букв")
