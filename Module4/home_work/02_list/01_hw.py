3088df6d8e39aafa2635f9d7cdfd1f07d660980a
SherGrigoriy <125800297+SherGrigoriy@users.noreply.github.com>
2023-03-10 22:24:17 +0300

    Update 01_hw.py

--- Module4/home_work/02_list/01_hw.py
+++ Module4/home_work/02_list/01_hw.py
@@ -2,8 +2,9 @@
 # Выведите все имена в одну строку через запятую
 
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
-
-# TODO: your code here
-
-# Пример вывода:
-# Иван, Ирина, Вячеслав, Василий, Петр
+# list_names = str(names)
+# format_str = list_names.replace("'", "")
+# format_str1 = format_str.replace("]", "")
+# format_str2 =  format_str1.replace("[", "")
+# print(format_str2)
+print(*names, sep=", ")
