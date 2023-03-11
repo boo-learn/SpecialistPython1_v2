0ec27dbf886863fa2d9c54649a963c3463dd24f3
Viktoriia Iushchenko <125797799+feldsherik@users.noreply.github.com>
2023-03-11 21:46:30 +0100

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,15 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = input('Введите текст:')
+vowels = ['а' 'о' 'у' 'ы' 'и' 'е' 'ё' 'э' 'ю''я']
+a = text.count('a')
+o = text.count('о')
+u = text.count('у')
+yi = text.count('ы')
+i = text.count('и')
+e = text.count('е')
+ye = text.count('ё')
+ee = text.count('э')
+yu = text.count('ю')
+ya = text.count('я')
+print(f"Согласных: {len(text) - (a + e + i + o + u + yi + ye + ee + yu + ya)}")
