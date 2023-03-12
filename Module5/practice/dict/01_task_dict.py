f5e75b08c2cea845897f888e26b9a6a6510eb4ad
kochergin579 <120784174+kochergin579@users.noreply.github.com>
2023-03-12 12:21:45 +0300

    Update 01_task_dict.py

--- Module5/practice/dict/01_task_dict.py
+++ Module5/practice/dict/01_task_dict.py
@@ -12,3 +12,12 @@ item_count = 8
 dollar_rate = 74.12
 
 # TODO: your code here
+item = {"name": "Кроссовки", "price": "7540.5", "currency": "rub"}
+item_count = 8
+dollar_rate = 74.12
+
+# Вычислите стоимость покупки в долларах
+price_dollar = float(item['price']) / dollar_rate
+
+summa = price_dollar * item_count
+print("%.2f" % summa)
