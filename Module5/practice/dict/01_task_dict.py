58a4eeb46dfd9b8b3ec24a9832c35da2d5bb0d98
Селиверстов Руслан <125797800+RuslanSeliverstov@users.noreply.github.com>
2023-03-12 12:30:26 +0300

    Update 01_task_dict.py

--- Module5/practice/dict/01_task_dict.py
+++ Module5/practice/dict/01_task_dict.py
@@ -11,4 +11,12 @@ item_count = 8
 # Курс доллара
 dollar_rate = 74.12
 
-# TODO: your code here
+item = {"name": "Кроссовки", "price": "7540.5", "currency": "rub"}
+item_count = 8
+dollar_rate = 74.12
+
+price_usd = round(float(item["price"]) / dollar_rate, 2)
+
+total_cost_usd = price_usd * item_count
+
+print(f"Стоимость {item_count} товаров '{item['name']}' в долларах: {total_cost_usd}")
