e6d4eea43d26525cfe24e5f79f19b01219dbc851
s16384 <126078219+s16384s@users.noreply.github.com>
2023-03-12 15:25:49 +0300

    Update 01_task_func.py

--- Module6/practice/funcs_part1/01_task_func.py
+++ Module6/practice/funcs_part1/01_task_func.py
@@ -2,7 +2,15 @@
 
 def max4(n1, n2, n3, n4):
     # TODO: your code here
-    pass
+   def max4(n1, n2, n3, n4):
+    if n1 >= n2 and n1 >= n3 and n1 >= n4:
+        return n1
+    if n2 >= n1 and n2 >= n3 and n2 >= n4:
+        return n2
+    if n3 >= n1 and n3 >= n2 and n3 >= n4:
+        return n3
+    if n4 >=n1 and n4 >= n2 and n4 >= n3:
+        return n4
 
 
 # Тестируем функцию
