6b9bddf729f7373b4d15fc52576ac57089a42f3f
upper87 <123666741+upper87@users.noreply.github.com>
2023-03-15 21:34:58 +0300

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,11 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = input("Введите строку: ")
+consonants = "бвгджзйклмнпрстфхцчшщ"
+sum = 0
+
+for glob in text:
+    for word in consonants:
+        if glob == word:
+            sum += 1
+print("Согласных букв:", sum)
