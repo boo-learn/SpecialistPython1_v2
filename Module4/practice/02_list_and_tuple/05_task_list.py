f0a6e1753c2eac43f01d68185bcfe93f464eca01
upper87 <123666741+upper87@users.noreply.github.com>
2023-03-15 16:13:40 +0300

    Update 05_task_list.py

--- Module4/practice/02_list_and_tuple/05_task_list.py
+++ Module4/practice/02_list_and_tuple/05_task_list.py
@@ -6,6 +6,31 @@
 fruits = ["яблоко", "банан", "киви", "арбуз"]
 
 # TODO: your code here
+fruits = ["яблоко", "банан", "киви", "арбуз", "картошечка", "марковка"]
+long_index = 0
+long_index_start = 0
+num = 0
+leng_fr = 0
+final_fr = ""
+space = " "
+looong_str = ""
+
+for leng in fruits:
+    long_index = len(leng)
+    if long_index_start < long_index:
+        long_index_start = long_index
+        looong_str = leng
+print(long_index_start)
+
+for list in fruits:
+    num += 1
+    leng_fr = len(list)
+    if leng_fr < long_index_start:
+        final_fr = (space * (long_index_start - len(list)) ) + list
+    else:
+        final_fr = looong_str
+
+    print(num, final_fr)
 
 # Пример вывода:
 # 1. яблоко
