723469ed9b578b5fac277dbc47f3dc6790b37bdf
bahaleksey <61473568+bahaleksey@users.noreply.github.com>
2023-03-15 16:13:55 +0300

    Update 06_task_list.py

--- Module4/practice/02_list_and_tuple/06_task_list.py
+++ Module4/practice/02_list_and_tuple/06_task_list.py
@@ -5,3 +5,21 @@ first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
 # TODO: your code here
+first_number = int(input("First Num: "))     # Первое число
+second_number = int(input("Second Num: "))    # Второе число
+
+#first_number = 0
+# second_number = 20
+i = 0
+j = 0
+if second_number > first_number:
+    i = first_number
+    j = second_number
+
+else:
+    i = second_number
+    j = first_number
+while i < j:
+    if i % 3 == 0 and i != 0:
+        print(i)
+    i += 1
