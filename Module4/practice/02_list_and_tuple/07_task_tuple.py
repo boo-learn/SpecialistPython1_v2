24c15e4be78d84cf9810917773c30d294d82a8d6
bahaleksey <61473568+bahaleksey@users.noreply.github.com>
2023-03-15 16:21:43 +0300

    Update 07_task_tuple.py

--- Module4/practice/02_list_and_tuple/07_task_tuple.py
+++ Module4/practice/02_list_and_tuple/07_task_tuple.py
@@ -3,3 +3,9 @@
 tup = (2, 4, 6, -4, 12, 0, 5)
 
 # TODO: your code here
+tup = (2, 4, 6, -4, 12, 0, 5)
+max_num_here = tup[0]
+for element in tup:
+    if element > max_num_here:
+        max_num_here = element
+print(max_num_here)
