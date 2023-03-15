94bf3698492eef7394563757c2cad87bbfe162ec
VladimirBudyak <127740435+VladimirBudyak@users.noreply.github.com>
2023-03-15 15:57:36 +0300

    Update 04_task_list.py

--- Module4/practice/02_list_and_tuple/04_task_list.py
+++ Module4/practice/02_list_and_tuple/04_task_list.py
@@ -2,3 +2,11 @@
 # Вывести на экран сумму всех положительных элементов.
 
 # TODO: your code here
+
+elements = [1, 7, 9, -9, 5, -3]
+
+result = 0
+for i_element in elements:
+    if i_element > 0:
+        result = result + i_element
+print(result)
