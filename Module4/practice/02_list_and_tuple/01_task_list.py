ce674a3e021ecff5a59ac386ed17d553c6ca8c3b
Nataroff <127740671+Nataroff@users.noreply.github.com>
2023-03-15 15:53:42 +0300

    Update 01_task_list.py

--- Module4/practice/02_list_and_tuple/01_task_list.py
+++ Module4/practice/02_list_and_tuple/01_task_list.py
@@ -4,3 +4,9 @@
 my_list = [24, "Василий", "выхухоль", 42, -12.5]
 
 # TODO: your code here
+my_list = [24, "Василий", "выхухоль", 42, -12.5]
+print(my_list[0])
+print(my_list[1])
+print(my_list[2])
+print(my_list[3])
+print(my_list[4])
