e2fbe53194be542484be663fb3358accb1c075ec
bahaleksey <61473568+bahaleksey@users.noreply.github.com>
2023-03-15 18:01:54 +0300

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,14 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = "В теории, теория и практика неразделимы. На практике это не так."
+consonants = "Б, В, Г, Д, Ж, З, Й, К, Л, М, Н, П, Р, С, Т, Ф, Х, Ц, Ч, Ш, Щ"
+vocabulary = consonants.replace(", ","")
+vocabulary = tuple(vocabulary)
+# конечно можно было сразу подать кортеж с буквами, но это первое из гугла и было интересно попробовать
+#print(vocabulary)
+i = 0
+for symbol in text:
+    if symbol.upper() in vocabulary:
+        #print(symbol, end='|')
+        i += 1
+print("Found", i, "symbols in text.")
