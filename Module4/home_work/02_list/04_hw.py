88922e714d404de63db796b815c2583a207b447e
VladimirBudyak <127740435+VladimirBudyak@users.noreply.github.com>
2023-03-16 12:08:30 +0300

    Update 04_hw.py

--- Module4/home_work/02_list/04_hw.py
+++ Module4/home_work/02_list/04_hw.py
@@ -5,3 +5,18 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+import random
+
+random_numbers = []
+n = 50
+for i in range(n):
+    random_numbers.append(random.randint(-100, 100))
+
+print(random_numbers)
+square_root_numbers = []
+for num in random_numbers:
+    if num > 0 and (num**(1/2)).is_integer():
+        square_root_numbers.append(int(num**(1/2)))
+
+print(square_root_numbers)
