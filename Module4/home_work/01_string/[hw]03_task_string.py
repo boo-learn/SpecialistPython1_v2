29633abc4ef065699ea8ddf78247174ac9d3dd49
AnnaAskarova <127740408+AnnaAskarova@users.noreply.github.com>
2023-03-16 07:14:05 +0500

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,15 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = "Мама мыла раму очень быстро"
+text=text.lower()
+consonants = "бвгджзйклмнпрстфхцчшщ"
+n = len(consonants)
+
+i = 0
+summa_consonants = 0
+
+while i < n:
+    amount = text.count(consonants[i])
+    i += 1
+    summa_consonants += amount
+print("Количество согласных: ", summa_consonants)
