30a20e93bdcc370565600f1b700a650a1d39daac
kerosinshik <47190739+kerosinshik@users.noreply.github.com>
2023-03-16 13:09:41 +0300

    Update 04_hw.py

--- Module4/home_work/02_list/04_hw.py
+++ Module4/home_work/02_list/04_hw.py
@@ -5,3 +5,25 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+import random
+
+n = int(input('Введите количество элементов: '))
+
+BEGINNING = -100
+FINISH = 100
+numbers = []
+i = 1
+
+while i <= n:
+    numbers.append(random.randint(BEGINNING, FINISH))
+    i += 1
+print('Список произвольных элементов:')
+print(numbers)
+
+results = []
+
+for j in numbers:
+    if j > 0 and ((j ** 0.5) * 10) % 10 == 0:
+        results.append(int(j ** 0.5))
+print()
+print('Результат: ', results)
