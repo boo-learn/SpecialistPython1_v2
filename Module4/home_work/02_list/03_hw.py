023d95133a2d7ee11a6260fee3753341277f4108
Chupazzz <127740333+Chupazzz@users.noreply.github.com>
2023-03-16 11:31:21 +0300

    Update 03_hw.py

--- Module4/home_work/02_list/03_hw.py
+++ Module4/home_work/02_list/03_hw.py
@@ -2,3 +2,21 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+n = int(input("Введите n: "))
+count = 0
+numbers = []
+positive_numbers = 0
+negative_numbers = 0
+
+import random
+
+while count < n:
+    numbers.append((random.randint(-100, 100)))
+    count +=1
+
+for number in numbers:
+
+    if number > 0 and number%2 == 0:
+        positive_numbers += number
+print(numbers)
+print(positive_numbers)
