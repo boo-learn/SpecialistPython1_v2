f754b5b94f0446660afbd81ee23f6b30e65b07f6
friendatatat <49976944+friendatatat@users.noreply.github.com>
2023-03-17 16:07:02 +0300

    Update 04_task_func.py

--- Module6/practice/funcs_part1/04_task_func.py
+++ Module6/practice/funcs_part1/04_task_func.py
@@ -3,10 +3,24 @@
 
 def can_triangle(p1, p2, p3):
     # TODO: your code here
-    pass
+    x1, y1 = p1
+    x2, y2 = p2
+    x3, y3 = p3
+    a = distance(x1, y1, x2, y2)
+    b = distance(x2, y2, x3, y3)
+    c = distance(x1, y1, x3, y3)
+    if ((a + b) > c) and ((b + c) > a) and ((a + c) > b):
+        return True
+    else:
+        return False
+
+
+def distance(x1, y1, x2, y2):
+    # TODO: your code here
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
 
 # Пример вызова функции
-can_triangle((10, 12), (14, 18), (12, 12))
+print(can_triangle((10, 12), (14, 18), (12, 12)))
 
 # Не забудьте протестировать вашу функцию
