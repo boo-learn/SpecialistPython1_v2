2c70b3d03319ced94b7188f6a232a111aea439c4
VladimirBudyak <127740435+VladimirBudyak@users.noreply.github.com>
2023-03-17 16:19:24 +0300

    Update 07_task_func.py

--- Module6/practice/funcs_part1/07_task_func.py
+++ Module6/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,14 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+
+def sec_to_time(s):
+    hours = s // 3600
+    tail = s % 3600
+    minutes = tail // 60
+    seconds = tail % 60
+    return (f"{hours:02d}:{minutes:02d}:{seconds:02d}")
+
+
+
+print(sec_to_time(29143))
