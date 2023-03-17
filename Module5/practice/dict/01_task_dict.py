d745562407f0b15d77af4ce7ee2e0950766873d8
pe1egrim86 <127744435+pe1egrim86@users.noreply.github.com>
2023-03-17 12:47:00 +0300

    Update 01_task_dict.py

--- Module5/practice/dict/01_task_dict.py
+++ Module5/practice/dict/01_task_dict.py
@@ -11,4 +11,12 @@ item_count = 8
 # Курс доллара
 dollar_rate = 74.12
 
-# TODO: your code here
+# Информация о товаре
+item = {"name": "Кроссовки", "price": "7540.5", "currency": "rub"}
+# Количество товаров
+item_count = 8
+# Курс доллара
+dollar_rate = 74.12
+
+final_price = (float(item_count) * dollar_rate * float(item["price"]))
+print(final_price)
