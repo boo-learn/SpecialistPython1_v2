8f5a859dc99bd5bdf1a926c7a7a5c23f4b03b38d
KUZNETSOV ALEKSANDR <125797734+Muragon84@users.noreply.github.com>
2023-03-17 16:53:06 +0300

    Update 02_hw_func.py

--- Module6/home_work/02_hw_func.py
+++ Module6/home_work/02_hw_func.py
@@ -5,9 +5,24 @@
 # При решении задачи необходимо использовать функцию расстояния между двумя точками.
 
 def distance(x1, y1, x2, y2):
-    # TODO: тело, которое вы реализовали на практической работе
-    pass
 
+    return (((x2 - x1) ** 2) + ((y2 - y1) ** 2)) ** 0.5
 
-# TODO: your code here
-print("Самый короткий отрезок:", ...)  # Выводим название отрезка, например “АС”.
+point_a = (2, 3)
+point_b = (7, -3)
+point_c = (5, 1)
+
+dic = { 'AB':0, 'CB':0, 'AC':0 }
+dic['AB'] = distance(*point_a, *point_b)
+dic['CB'] = distance(*point_c, *point_b)
+dic['AC'] = distance(*point_a, *point_c)
+dic_min = sorted(dic.values())
+dic_min_sorted = {}
+
+for i in dic_min:
+    for k in dic.keys():
+        if dic[k] == i:
+            dic_min_sorted[k] = dic[k]
+            break
+print(dic_min_sorted)
+print("Самый короткий отрезок:", list(dic_min_sorted.keys())[0], list(dic_min_sorted.values())[0])
