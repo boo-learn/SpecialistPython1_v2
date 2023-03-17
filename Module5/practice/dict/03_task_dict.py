7b2f687defd2223a83c9c2d7006412d0c3b65f89
AnnaAskarova <127740408+AnnaAskarova@users.noreply.github.com>
2023-03-17 15:10:17 +0500

    Update 03_task_dict.py

--- Module5/practice/dict/03_task_dict.py
+++ Module5/practice/dict/03_task_dict.py
@@ -4,6 +4,9 @@
 keys = ['name', 'surname', 'age', 'rate']
 values = ['Петр', 'Первый', 42, 1300]
 
+user = dict(zip(keys, values))
+
+print(user)
 # TODO: your code here
 # Нужно получить словарь:
 # {'name': 'Петр', 'surname': 'Первый', 'age': 42, 'rate': 1300}
