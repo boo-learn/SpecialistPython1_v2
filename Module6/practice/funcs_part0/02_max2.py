8a20e64a5d5f36b1953c4d82ed08eac58985de81
naatalik <127740269+naatalik@users.noreply.github.com>
2023-03-17 15:08:33 +0300

    Update 02_max2.py

--- Module6/practice/funcs_part0/02_max2.py
+++ Module6/practice/funcs_part0/02_max2.py
@@ -4,7 +4,10 @@ def max2(n1, n2):
     # TODO: your code here
     pass
 
-
+def max2(n1,  n2):
+    if n1 < n2:
+        return n2
+    return n1
 # Тестируем функцию
 print(max2(5, 6))
 print(max2(-10, -12))
