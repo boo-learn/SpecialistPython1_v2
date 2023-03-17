f4520ea354d13a773806ba59edc80c914a55c17a
Nataroff <127740671+Nataroff@users.noreply.github.com>
2023-03-17 15:30:35 +0300

    Update 01_task_func.py

--- Module6/practice/funcs_part1/01_task_func.py
+++ Module6/practice/funcs_part1/01_task_func.py
@@ -1,8 +1,16 @@
 # Напишите функцию, возвращающую наибольшее из четырех чисел
 
 def max4(n1, n2, n3, n4):
-    # TODO: your code here
-    pass
+    # TODO:
+    def max4(n1, n2, n3, n4):
+    if n1 >= n2 and n1 >= n3 and n1 >= n4:
+        return n1
+    if n2 >= n1 and n2 >= n3 and n2 >= n4:
+        return n2
+    if n3 >= n2 and n3 >= n4 and n3 >= n1:
+        return n3
+    if n4 >= n2 and n4 >= n3 and n4 >= n1:
+        return n4
 
 
 # Тестируем функцию
