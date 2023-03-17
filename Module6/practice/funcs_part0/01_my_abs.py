f4c7caa97eb182988a52a6e43a827c56de96eacb
kerosinshik <47190739+kerosinshik@users.noreply.github.com>
2023-03-17 14:56:22 +0300

    Update 01_my_abs.py

--- Module6/practice/funcs_part0/01_my_abs.py
+++ Module6/practice/funcs_part0/01_my_abs.py
@@ -6,3 +6,14 @@ def my_abs(...):
 print(my_abs(-5))
 print(my_abs(5))
 print(my_abs(0))
+
+
+def my_abs(a):
+    if a < 0:
+        return -a
+    else:
+        return a
+
+print(my_abs(-5))
+print(my_abs(5))
+print(my_abs(0))
