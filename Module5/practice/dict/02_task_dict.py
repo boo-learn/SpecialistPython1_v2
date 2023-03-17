30852b14c7b24b90f159a29776f10e4ee8fbcc57
SergeiKovantsev <127740704+SergeiKovantsev@users.noreply.github.com>
2023-03-17 12:59:56 +0300

    Update 02_task_dict.py

--- Module5/practice/dict/02_task_dict.py
+++ Module5/practice/dict/02_task_dict.py
@@ -10,3 +10,17 @@ items = [
 ...
 # 2. Выведите цену самого дешевого товара
 ...
+items = [
+    {"name": "Кроссовки", "price": "7540.5", "currency": "rub"},
+    {"name": "Шорты", "price": "1313.2", "currency": "rub"},
+    {"name": "Кепка", "price": "738.0", "currency": "rub"},
+    {"name": "Чемодан", "price": "2300.85", "currency": "rub"},
+]
+for num, item in enumerate(items, 1):
+    print(f"{num}. {item['name']}")
+
+min_price = float(items[0]["price"])
+for item in items:
+    if float(item["price"]) < min_price:
+        min_price = float(item["price"])
+print(min_price)
