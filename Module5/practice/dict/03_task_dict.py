853e676f13b7937b8bfa5d8b11d6d5919521e567
Tata65 <127740708+Tata65@users.noreply.github.com>
2023-03-17 13:02:14 +0300

    Update 03_task_dict.py

--- Module5/practice/dict/03_task_dict.py
+++ Module5/practice/dict/03_task_dict.py
@@ -7,3 +7,6 @@ values = ['Петр', 'Первый', 42, 1300]
 # TODO: your code here
 # Нужно получить словарь:
 # {'name': 'Петр', 'surname': 'Первый', 'age': 42, 'rate': 1300}
+people = dict(zip(keys, values))
+
+print(people)
