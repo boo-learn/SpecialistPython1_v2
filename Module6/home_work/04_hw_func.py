6f938131740c6ac0ceacfadc4047307c692bc34b
SherGrigoriy <125800297+SherGrigoriy@users.noreply.github.com>
2023-03-18 23:19:43 +0300

    Update 04_hw_func.py

--- Module6/home_work/04_hw_func.py
+++ Module6/home_work/04_hw_func.py
@@ -9,6 +9,44 @@
 # Ввод: -2/3 - -2
 # Вывод: 1 1/3
 
-# TODO: your code here
+fraction = input('Введите условие:')
+first_number = fraction[0:fraction.find(' ')]
+second_number = fraction[:fraction.find(' ') + 2:-1][::-1]
+sign = fraction[fraction.find(' '): -1][1]
+if first_number.find('/') != -1:
+    frst_denominator = first_number[first_number.find('/') + 1:]
+    frst_numerator = first_number[:first_number.find('/')]
+    if second_number.find('/') != -1:
+        scnd_denominator = second_number[second_number.find('/') + 1:]
+        scnd_numerator = second_number[:second_number.find('/')]
+        if frst_denominator != scnd_denominator:
+            common_denominator = int(frst_denominator) * int(scnd_denominator)
+            numerator = int(frst_numerator) * int(scnd_denominator) + int(scnd_numerator) * int(frst_denominator)
+            whole = numerator // common_denominator
+            print(str(whole), " ", numerator % common_denominator, "/", common_denominator, sep='')
+        elif frst_denominator == scnd_denominator:
+            common_denominator = int(frst_denominator)
+            numerator = int(frst_numerator) + int(scnd_numerator)
+            whole = numerator // common_denominator
+            print(str(whole), " ", numerator % common_denominator, "/", common_denominator, sep='')
+    elif second_number.find('/') == -1:
+        scnd_denominator = int(frst_denominator)
+        scnd_numerator = int(second_number) * int(frst_denominator)
+        common_denominator = int(frst_denominator)
+        numerator = int(frst_numerator) + int(scnd_numerator)
+        whole = numerator // common_denominator
+        print(str(whole), " ", numerator % common_denominator, "/", common_denominator, sep='')
+elif first_number.find('/') == -1:
+    if second_number.find('/') != -1:
+        scnd_denominator = second_number[second_number.find('/') + 1:]
+        scnd_numerator = second_number[:second_number.find('/')]
+        frst_denominator = int(scnd_denominator)
+        frst_numerator = int(first_number) * int(scnd_denominator)
+        common_denominator = int(scnd_denominator)
+        numerator = int(frst_numerator) + int(scnd_numerator)
+        whole = numerator // common_denominator
+        print(str(whole), " ", numerator % common_denominator, "/", common_denominator, sep='')
+    else:
+        print(int(first_number) + int(second_number))
 
 
