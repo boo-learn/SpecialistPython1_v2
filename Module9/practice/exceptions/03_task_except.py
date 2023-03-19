faf88a36f57e43623953dd09580fada2e5d3eebb
Anton Mandrykin <classickslk@gmail.com>
2023-03-19 15:30:27 +0300

    Update 03_task_except.py

--- Module9/practice/exceptions/03_task_except.py
+++ Module9/practice/exceptions/03_task_except.py
@@ -5,3 +5,25 @@
 
 # При решении задачи требуется учесть формат входных данных.
 # Если входные данные некорректные, сообщить об этом.
+
+
+input_str = "2 12 -45 7 10"
+try:
+    numbers = list(map(int, input_str.split()))
+    if len(numbers) != 5:
+        raise ValueError('Входная строка должна содержать ровно 5 чисел')
+
+    min_positive = None
+    for number in numbers:
+        if number > 0 and (min_positive is None or number < min_positive):
+            min_positive = number
+
+    if min_positive is None:
+        print('Нет положительных чисел во входных данных')
+    else:
+        print(min_positive)
+
+except ValueError as e:
+    print(f'Ошибка: {e}')
+except Exception as e:
+    print(f'Неожиданная ошибка: {e}')
