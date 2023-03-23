cb73bf84a67a56c2d5aa9d2bce54921d02a1bde2
bahaleksey <61473568+bahaleksey@users.noreply.github.com>
2023-03-23 15:33:32 +0300

    Update 04_task_dict.py
    Last task - Sorting

--- Module5/practice/dict/04_task_dict.py
+++ Module5/practice/dict/04_task_dict.py
@@ -29,20 +29,56 @@ staff = [
 # Вычислите:
 print("Имя и Фамилию сотрудника с самой высокой зарплатой:")
 
-# TODO: your code here
+max_salary = staff[0]["salary"]
+max_salary_staff = staff[0]
+for item in staff:
+    # print(item)
+    if float(item["salary"]) > max_salary:
+        max_salary = float(item["salary"])
+        max_salary_staff = item
+print(f"{max_salary_staff['surname']} {max_salary_staff['name']} have max salary {max_salary_staff['salary']}")
 
 print("Имя и Фамилию сотрудника с самой низкой зарплатой:")
-
-# TODO: your code here
+min_salary = staff[0]["salary"]
+min_salary_staff = staff[0]
+for item in staff:
+    # print(item)
+    if float(item["salary"]) < min_salary:
+        min_salary = float(item["salary"])
+        min_salary_staff = item
+print(f"{min_salary_staff['surname']} {min_salary_staff['name']} have min salary {min_salary_staff['salary']}")
 
 print("Среднеарифметическую зарплату всех сотрудников")
 
-# TODO: your code here
+summ_salary = 0
+for item in staff:
+    summ_salary += float(item["salary"])
+# print(len(staff))
+# print(summ_salary)
+print(summ_salary / len(staff))
 
 print("Количество однофамильцев в организации")
-
-# TODO: your code here
+counter = 0
+for num, item in enumerate(staff, 1):
+    for num_incl, item_incl in enumerate(staff, 1):
+        if num != num_incl and item["surname"] == item_incl["surname"]:
+            counter += 1
+print(counter / 2)  # same pairs
 
 print("*Список всех сотрудников(Имя и Фамилию) в порядке возрастания их зарплаты")
 
-# TODO: your code here
+staff_pre_sorted = staff
+staff_sorted = []
+while len(staff_pre_sorted) > 0:
+    min_salary = float(staff_pre_sorted[0]["salary"])
+    min_salary_staff = staff_pre_sorted[0]
+    min_salary_staff_index = 0
+    for num, item in enumerate(staff_pre_sorted, 0):
+        if float(item["salary"]) < min_salary:
+            min_salary = float(item["salary"])
+            min_salary_staff = item
+            min_salary_staff_index = num
+    staff_sorted.append(min_salary_staff)
+    staff_pre_sorted.pop(min_salary_staff_index)
+
+print(staff_sorted)
