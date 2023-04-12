42811c2ac3d5e5e794893e83d898f967df0aea0d
AndreyAndrew <kv58@bk.ru>
2023-04-12 19:41:42 +0300

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,11 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = "выарвор авпор авогапо апоао карот"
+
+letters = ("б", "в", "г", "д", "ж", "з", "й", "к", "л", "м", "н", "п", "р", "с", "т", "ф", "х", "ц", "ч", "ш", "щ")
+count_letters = 0
+
+for letter in letters:
+    count_letters += letters.count(letter)
+
+print(count_letters)
