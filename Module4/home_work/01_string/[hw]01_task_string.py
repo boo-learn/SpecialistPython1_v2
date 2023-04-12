732f3948536d609c636647d72585cba6a6089d64
Nikoaly <74960807+Nikoaly@users.noreply.github.com>
2023-04-12 21:29:55 +0300

    Update [hw]01_task_string.py

--- Module4/home_work/01_string/[hw]01_task_string.py
+++ Module4/home_work/01_string/[hw]01_task_string.py
@@ -1,4 +1,13 @@
 # Дан произвольный текст. Каких символов больше, точек "." или запятых ","?
 # Если точек и запятых одинаково - выведите "одинаково"
 
-text = ...
+text = input("Введите произвольный текст: ")
+count_dot = text.count('.')
+count_comma = text.count(',')
+
+if count_dot > count_comma:
+    print("Больше точек")
+elif count_dot < count_comma:
+    print("Больше запятых")
+else:
+    print("Одинаково")
