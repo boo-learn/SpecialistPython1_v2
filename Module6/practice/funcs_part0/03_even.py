fbcf38831119858006cb05eb0db9357689c4c7f4
Nikoaly <74960807+Nikoaly@users.noreply.github.com>
2023-04-13 11:41:00 +0300

    Update 03_even.py

--- Module6/practice/funcs_part0/03_even.py
+++ Module6/practice/funcs_part0/03_even.py
@@ -1,9 +1,13 @@
-def even(...):
-    ...
-    return ...
+def even(n):
+    # Проверяем, делится ли число на 2 без остатка
+    if n % 2 == 0:
+        return True
+    else:
+        return False
 
-n = ...
+n = int(input("Введите целое число: "))
+
 if even(n):
-   print("Число четное")
+    print("Число четное")
 else:
-   print("Число не четное")
+    print("Число не четное")
