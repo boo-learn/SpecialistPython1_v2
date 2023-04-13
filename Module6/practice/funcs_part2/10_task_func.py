d1f5d777b4732e6c4c14199f2d6818eb4009385d
dsyutov <130360288+dsyutov@users.noreply.github.com>
2023-04-13 14:28:51 +0300

    Update 10_task_func.py

--- Module6/practice/funcs_part2/10_task_func.py
+++ Module6/practice/funcs_part2/10_task_func.py
@@ -2,8 +2,12 @@
 # Функция должна вызываться с любым количеством аргументов
 
 def average(*args):
-    # TODO: your code here
-    pass
+    i = 0
+    aver = 0
+    for number in args:
+        i += 1
+        aver += number
+    return aver / i
 
 
 print(average(3, 4, 8))
