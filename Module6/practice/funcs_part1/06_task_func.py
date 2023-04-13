1b3f96adbf28a7b1c778e3617059942656abe269
PythonEngine <130360297+PythonEngine@users.noreply.github.com>
2023-04-13 14:03:24 +0300

    Update 06_task_func.py

--- Module6/practice/funcs_part1/06_task_func.py
+++ Module6/practice/funcs_part1/06_task_func.py
@@ -2,7 +2,17 @@
 # находится строго внутри окружности с центром в точке (xc, yc) и радиусом r:
 
 def point_in_circle(x, y, xc, yc, r):
-    # TODO: your code here
-    pass
+    return abs(x - xc) < r and abs(y - yc) < r
+
+
+point = (10, 10)
+center = (5, 5)
+radius = 2
+
+if point_in_circle(*point, *center, radius):
+    print("Точка находится внутри окружности")
+else:
+    print("Точка не находится внутри окружности")
+
 
 # Не забудьте протестировать вашу функцию
