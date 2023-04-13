0a5b0e92c15b0d1ca7270df8cce5f3356b16fd1c
platankov <130360357+platankov@users.noreply.github.com>
2023-04-13 20:22:15 +0300

    Update 01_hw_func.py

--- Module6/home_work/01_hw_func.py
+++ Module6/home_work/01_hw_func.py
@@ -3,8 +3,19 @@
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 
 def lucky_ticket(ticket_number):
-    # TODO: your code here
-    pass
+    if len(str(ticket_number)) == 6:
+        digit_1 = int(str(ticket_number)[0])
+        digit_2 = int(str(ticket_number)[1])
+        last_digit = int(str(ticket_number)[-1])
+        pre_last_digit = int(str(ticket_number)[-2])
+
+        if digit_1 + digit_2 == pre_last_digit + last_digit:
+            result = "Счастливый билет !"
+        else:
+            result = "Несчастный  билет !"
+    else:
+        result = "Должно быть 6 цифр !"
+    return result
 
 
 # Тестируем функцию
