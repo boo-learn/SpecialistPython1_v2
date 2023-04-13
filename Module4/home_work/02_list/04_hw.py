00c7b89b79031fa9324993362d73e401f69c2bc5
KindJohnny <130360347+KindJohnny@users.noreply.github.com>
2023-04-13 10:12:59 +0300

    Update 04_hw.py

--- Module4/home_work/02_list/04_hw.py
+++ Module4/home_work/02_list/04_hw.py
@@ -5,3 +5,23 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+import random
+
+numbers = []
+squares = []
+
+num_elems = 25 #int(input("Кол-во элементов:"))
+
+for i in range(num_elems):
+    numbers.append(random.randint(-100, 100))
+
+print(numbers)
+
+for elem in  numbers:
+    if elem > 0:
+        sq = elem**0.5
+        if  not (sq - int(sq)):
+            squares.append(int(sq))
+
+print(squares)
