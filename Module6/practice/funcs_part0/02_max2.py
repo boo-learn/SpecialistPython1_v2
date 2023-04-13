1b25969c57e2c010a523199570272a0788461b5b
dsyutov <130360288+dsyutov@users.noreply.github.com>
2023-04-13 11:33:54 +0300

    Update 02_max2.py

--- Module6/practice/funcs_part0/02_max2.py
+++ Module6/practice/funcs_part0/02_max2.py
@@ -1,11 +1,11 @@
-# Напишите функцию, возвращающую наибольшее из двух чисел
-
 def max2(n1, n2):
-    # TODO: your code here
+    if n1 > n2:
+        return n1
+    else:
+        return n2
     pass
 
 
-# Тестируем функцию
 print(max2(5, 6))
 print(max2(-10, -12))
 print(max2(2.5, 2.6))
