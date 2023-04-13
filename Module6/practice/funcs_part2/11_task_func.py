e079b0df087544fbeb2aa59a753f88cad2ed59d6
Nikoaly <74960807+Nikoaly@users.noreply.github.com>
2023-04-13 14:30:03 +0300

    Update 11_task_func.py

--- Module6/practice/funcs_part2/11_task_func.py
+++ Module6/practice/funcs_part2/11_task_func.py
@@ -1,18 +1,14 @@
 # Используя функцию average() из предыдущей задачи, найдите среднее арифметическое всех элементов списка и кортежа
 
 def average(*args):
-    # TODO: тело из предыдущей задачи
+    if not args:
+        return None
+    return sum(args) / len(args)
     pass
 
 
 def gen_list(size, at=-10, to=10):
-    import random  # импорт в функции возможен, но не рекомендуется PEP-8
-    """
-    :param size: кол-во элементов списка
-    :param at: минимально возможное значение элементов
-    :param to: максимально возможное значение элементов
-    :return: списко из size произвольных элементов вдиапазоне at..to 
-    """
+    import random
     result_list = []
     for _ in range(size):
         result_list.append(random.randint(at, to))
@@ -21,3 +17,9 @@ def gen_list(size, at=-10, to=10):
 
 my_list = gen_list(10)
 my_tuple = 5, 7, -4, 10, 8
+
+avg_list = average(*my_list)
+avg_tuple = average(*my_tuple)
+
+print(f"Среднее арифметическое списка {my_list}: {avg_list}")
+print(f"Среднее арифметическое кортежа {my_tuple}: {avg_tuple}")
