2ae9b526f91b23589a2e7f58e4b50a9c99652996
AndreyAndrew <kv58@bk.ru>
2023-04-13 14:23:27 +0300

    Update 11_task_func.py

--- Module6/practice/funcs_part2/11_task_func.py
+++ Module6/practice/funcs_part2/11_task_func.py
@@ -1,8 +1,7 @@
 # Используя функцию average() из предыдущей задачи, найдите среднее арифметическое всех элементов списка и кортежа
 
 def average(*args):
-    # TODO: тело из предыдущей задачи
-    pass
+    return sum(args)/len(args)
 
 
 def gen_list(size, at=-10, to=10):
@@ -21,3 +20,5 @@ def gen_list(size, at=-10, to=10):
 
 my_list = gen_list(10)
 my_tuple = 5, 7, -4, 10, 8
+
+print(average(*my_tuple, *my_list))
