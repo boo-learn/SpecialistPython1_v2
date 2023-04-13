088a87cd896cb3855b797158329881ede775e5ed
PythonEngine <130360297+PythonEngine@users.noreply.github.com>
2023-04-13 19:45:32 +0300

    Update 01_hw_func.py

--- Module6/home_work/01_hw_func.py
+++ Module6/home_work/01_hw_func.py
@@ -2,12 +2,26 @@
 # Решение реализовать в виде функции.
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 
+def str_sum(s):
+    i = 0
+    total = 0
+    while i < len(s):
+        total += int(s[i])
+        i += 1
+    return total
+
+
 def lucky_ticket(ticket_number):
-    # TODO: your code here
-    pass
+    t = str(ticket_number)
+    if len(t) == 6:
+        return str_sum(t[:3]) == str_sum(t[3:])
+    else:
+        return False
 
 
 # Тестируем функцию
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
 print(lucky_ticket(436751))
+print(lucky_ticket(936751))
+print(lucky_ticket(135351))
