41f7c318aa8e02afc34911e063830eff12533751
juliagoold <36034434+juliagoold@users.noreply.github.com>
2023-04-13 11:24:48 +0300

    Update 01_my_abs.py

--- Module6/practice/funcs_part0/01_my_abs.py
+++ Module6/practice/funcs_part0/01_my_abs.py
@@ -1,8 +1,12 @@
-def my_abs(...):
-    ...
-    return ...
+def my_abs(num):
+    if num >= 0:
+        return num
+    else:
+        return -num
 
 
 print(my_abs(-5))
 print(my_abs(5))
 print(my_abs(0))
+
+
