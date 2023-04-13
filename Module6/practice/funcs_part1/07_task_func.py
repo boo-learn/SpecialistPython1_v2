58f557ec58d472417a9e90756a97294e3ec33db8
PythonEngine <130360297+PythonEngine@users.noreply.github.com>
2023-04-13 14:39:12 +0300

    Update 07_task_func.py

--- Module6/practice/funcs_part1/07_task_func.py
+++ Module6/practice/funcs_part1/07_task_func.py
@@ -1,4 +1,15 @@
-# Напишите функцию принимающую время в секундах и возвращающую строку формата: “hh:mm:ss”,
-# где hh - часы, mm- минуты, ss - секунды.
-# Пример:
-# 29143 секунд → 08:05:43
+def format_time(s):
+    hh = s // (60 * 60) % 24
+    mm = (s - hh * 60 * 60) // 60 % 60
+    ss = (s - hh * 60 * 60 - mm * 60) % (24 * 60 * 60)
+    return f"{hh:02}:{mm:02}:{ss:02}"
+
+
+print(format_time(13))
+print(format_time(130))
+print(format_time(3601))
+print(format_time(40271))
+print(format_time(45296))
+print(format_time(86399))
+print(format_time(86400))
+print(format_time(86401))
