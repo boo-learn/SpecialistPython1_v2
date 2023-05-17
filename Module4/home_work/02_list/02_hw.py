167da83a97cf14edeeb076cdae2096ac3ad19c03
ZhukMV <133659486+ZhukMV@users.noreply.github.com>
2023-05-17 18:31:04 +0300

    Update 02_hw.py

--- Module4/home_work/02_list/02_hw.py
+++ Module4/home_work/02_list/02_hw.py
@@ -2,7 +2,10 @@
 # в диапазоне от -100 до 100. В списке должно быть n - элементов.
 # Подсказка:
 # для получения случайного числа используйте функцию randint() модуля random
-import random
-numbers = []
+from random import randint
+
+numbers = [randint(-100, 100) for _ in range(int(input("n: "))) ]
+
+print(numbers)
 # print(random.randint(10, 20))  # раскомментируйте, чтобы посмотреть работу функции randint
 # TODO: your code here
