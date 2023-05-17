f3cbf315e56827d17ad5ad3c088f940e6cb927e9
EProkopeva1012 <133659236+EProkopeva1012@users.noreply.github.com>
2023-05-17 16:19:44 +0300

    Update 06_task_list.py

--- Module4/practice/02_list_and_tuple/06_task_list.py
+++ Module4/practice/02_list_and_tuple/06_task_list.py
@@ -4,4 +4,10 @@
 first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
-# TODO: your code here
+
+index = first_number
+while index < second_number:
+    if index % 3 == 0:
+        print(index)
+    index = index +1
+
