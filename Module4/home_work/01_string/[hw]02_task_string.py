99b9a942a8601232cbd49f89a398499302e36e4e
Tmanvelova <133658883+Tmanvelova@users.noreply.github.com>
2023-05-18 00:08:14 +0300

    Update [hw]02_task_string.py

--- Module4/home_work/01_string/[hw]02_task_string.py
+++ Module4/home_work/01_string/[hw]02_task_string.py
@@ -1,3 +1,13 @@
 # Дана строка состоящая из слов, все слова разделены ровно одним пробелом. Знаки препинания отсутствуют.
 # Найдите первое слово, начинающееся на букву "б"
 # если слова на эту букву нет, выведите "слов на б нет"
+line = input("Введите строку: ")
+first_letter_in_word = "б"
+number = 0
+
+for word in line.split():
+    if word[0] == first_letter_in_word and number == 0:
+        print(word)
+        number += 1
+if number == 0:
+    print(f"слов на {first_letter_in_word} нет")
