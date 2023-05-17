0d323749ea842c1383ee2a7d2b189c293daed204
ElenaDrobina <108633214+ElenaDrobina@users.noreply.github.com>
2023-05-18 01:16:55 +0500

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,10 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = input('text: ')
+vowels = ['а', 'о', 'и', 'ы', 'у', 'э', 'я', 'е']
+letter_count = 0
+count = len(text)
+for letter in text:
+    if letter in vowels:
+        letter_count += 1
+print(count - letter_count)
