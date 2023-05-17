10def803a347f1cdb869ba20201359d92fd4593c
ElenaDrobina <108633214+ElenaDrobina@users.noreply.github.com>
2023-05-18 01:05:22 +0500

    Update [hw]02_task_string.py

--- Module4/home_work/01_string/[hw]02_task_string.py
+++ Module4/home_work/01_string/[hw]02_task_string.py
@@ -1,3 +1,15 @@
 # Дана строка состоящая из слов, все слова разделены ровно одним пробелом. Знаки препинания отсутствуют.
 # Найдите первое слово, начинающееся на букву "б"
 # если слова на эту букву нет, выведите "слов на б нет"
+text = input('text: ')
+words = text.lower().split()
+for word in words:
+    if word[0] == 'б':
+        conclusion = word
+        break
+    else:
+        conclusion = ''
+if conclusion == '':
+    print('слов на б нет')
+else:
+    print(word)
