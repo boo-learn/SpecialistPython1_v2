3a43567790d227acc597f373cc6619a6ad477457
ZhukMV <133659486+ZhukMV@users.noreply.github.com>
2023-05-17 18:55:58 +0300

    Update 04_hw.py

--- Module4/home_work/02_list/04_hw.py
+++ Module4/home_work/02_list/04_hw.py
@@ -5,3 +5,14 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+from random import randint
+
+init_list = [randint(-100, 100) for i in range(int(input("n: ")))]
+sqrt_list = []
+
+for number in init_list:
+    if number > 0 and number ** 0.5 % 1 == 0:
+        sqrt_list.append(int(number ** 0.5))
+print(init_list)
+print(sqrt_list)
