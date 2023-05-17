15eda519e486eb99f024848a34e36134badfefe7
BelousovAlexey <133659407+BelousovAlexey@users.noreply.github.com>
2023-05-17 15:43:35 +0300

    Update 05_task_list.py

--- Module4/practice/02_list_and_tuple/05_task_list.py
+++ Module4/practice/02_list_and_tuple/05_task_list.py
@@ -5,7 +5,35 @@
 # Исходные данные:
 fruits = ["яблоко", "банан", "киви", "арбуз"]
 
-# TODO: your code here
+fruits = ["яблоко", "банан", "киви", "арбуз"]
+
+len_frut_name = [0]
+#len_frut_name.append(1)
+#print (len_frut_name)
+#len_frut_name.sort()
+#print(len_frut_name)
+
+
+
+for frut in (fruits):
+    len_frut_name.append(len(frut))
+#print(len_frut_name)
+len_frut_name.sort(reverse=True)
+
+#print(len_frut_name)
+
+max_len = len_frut_name[0]
+#print(max_len)
+
+i = 0
+for frut in (fruits):
+   if len(frut) > 0:
+      #  len_frut_name = len_frut_name +
+  #  print(len(frut))
+   # print(frut)
+    i +=1
+    print(i,frut.rjust(max_len, " "))
+
 
 # Пример вывода:
 # 1. яблоко
