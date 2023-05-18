0e733523f831c164b2a8a1dcdfe60dc4fdcb53af
bhalex <133658851+bhalex@users.noreply.github.com>
2023-05-18 15:13:38 +0300

    Update 07_task_func.py

--- Module6/practice/funcs_part1/07_task_func.py
+++ Module6/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,13 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+
+def get_formatted_time(s: int) -> str:
+    hours = s % 86400 // 3600
+    minutes = s % 86400 % 3600 // 60
+    seconds = s % 60
+    return f"{hours:0>2}:{minutes:0>2}:{seconds:0>2}"
+
+
+seconds = int(input("Прошло секунд всего: "))
+print(get_formatted_time(seconds))
