2cfffede0bc48961de7d2fc8c6cf3d26107031f5
Aniskaya <38287381+Aniskaya@users.noreply.github.com>
2023-05-18 14:09:28 +0300

    Update 01_task_func.py

--- Module6/practice/funcs_part1/01_task_func.py
+++ Module6/practice/funcs_part1/01_task_func.py
@@ -1,11 +1,16 @@
 # Напишите функцию, возвращающую наибольшее из четырех чисел
 
 def max4(n1, n2, n3, n4):
-    # TODO: your code here
-    pass
+    max_val = n1
+    a = [n1, n2, n3, n4]
+    for num in a:
+        if num > max_val:
+            max_val = num
+        else:
+            max_val = max_val
+    return max_val
 
 
-# Тестируем функцию
 print(max4(5, 6, 12, 7))
 print(max4(-10, -12, -4, -9))
 print(max4(2.5, 2.6, 2.6, 2.4))
