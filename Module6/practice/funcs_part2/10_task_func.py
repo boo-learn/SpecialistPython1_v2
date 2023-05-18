69c752fa4bc0b1712b6e57b01f66f0d6907b0f1c
AndreyChernavin <133659172+AndreyChernavin@users.noreply.github.com>
2023-05-18 16:14:56 +0300

    Update 10_task_func.py

--- Module6/practice/funcs_part2/10_task_func.py
+++ Module6/practice/funcs_part2/10_task_func.py
@@ -1,9 +1,14 @@
 # Напишите функцию, находящую среднее арифметическое всех аргументов
 # Функция должна вызываться с любым количеством аргументов
 
-def average(*args):
-    # TODO: your code here
-    pass
+def average(*args) -> float:
+    count_args = 0
+    summa = 0
+    for arg in args:
+        summa += arg
+        count_args += 1
+
+    return (summa / count_args)
 
 
 print(average(3, 4, 8))
