07f304be49edcef63e0bac1b033c7247c7914579
Naff10284 <127292197+Naff10284@users.noreply.github.com>
2023-05-18 11:43:47 +0300

    Update 02_task_dict.py

--- Module5/practice/dict/02_task_dict.py
+++ Module5/practice/dict/02_task_dict.py
@@ -10,3 +10,18 @@ items = [
 ...
 # 2. Выведите цену самого дешевого товара
 ...
+
+items = [
+    {"name": "Кроссовки", "price": "7540.5", "currency": "rub"},
+    {"name": "Шорты", "price": "1313.2", "currency": "rub"},
+    {"name": "Кепка", "price": "738.0", "currency": "rub"},
+    {"name": "Чемодан", "price": "2300.85", "currency": "rub"},
+]
+lower_price = 0
+for item in items:
+    print (enumerate (item["name"]))
+    
+    if lower_price < float(item["price"]):
+       lower_price = float(item["price"])
+       
+print(lower_price )
